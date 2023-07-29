ee9079536df269beaeb0d9178eeddab09d28910f
Tricia <46463360+triciadang@users.noreply.github.com>
2023-07-29 11:46:54 -0400

    PEX3

---
+++ CompletedPEX3/2 - AddMult - Parser/dang-SableCC.bat
@@ -0,0 +1,13 @@
+:: * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+:: * This file is part of SableCC.                             *
+:: * See the file "LICENSE" for copyright information and the  *
+:: * terms and conditions for copying, distribution and        *
+:: * modification of SableCC.                                  *
+:: * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+::
+:: IMPORTANT: Put the absolute path to sablecc.jar below.
+::
+:: java -jar "LOCATION_OF_SABLECC.JAR-MAY_REQUIRE_QUOTES" -t csharp,csharp-build NAME_OF_GRAMMAR_FILE
+
+java -jar "C:\Users\C20Tricia.Dang\Documents\Compilers\sablecc-3-beta.3.altgen.20041114\sablecc-3-beta.3.altgen.20041114\lib\sablecc.jar" -t csharp,csharp-build simple.grammar
+pause
